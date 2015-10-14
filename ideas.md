9280b028fb7705799e56a004ddb67b4d07a58682
Nacho <mirallei@uji.es>
2015-10-14 14:30:45 +0200

    Marker tested and minor analysis

--- ideas.md
+++ ideas.md
@@ -1,18 +1,3 @@
-#SocketMap Project.
-In this project we're trying to work con the Socket.IO library and creating a GIS Application capable to show users where the other users are in the moment the acces the browser.
-Is is based on this technologies:
-
-* [Node.js](https://nodejs.org/en/)
-* [MongoDB](https://www.mongodb.org/)
-* [Backbone](http://backbonejs.org/)
-* [Express](http://expressjs.com/)
-* [Socket,IO](http://socket.io/)
-* [Mongoose](http://mongoosejs.com/)
-
-The ideea is finishing this project by crreating the IOs and Android APPS, allow creating groups and add some security.
-
-Here comes some preview <b>analisys</b>:
-
 ##Data Model.
 Describing the way we organice the users.
 
--- public/mapCode.js
+++ public/mapCode.js
@@ -1,21 +1,40 @@
 /**
  * Created by NachoGeotec on 06/10/2015.
  */
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+
 
 var username = null;
 var lat = null;
 var lon = null;
 var socket = null;
 var users = [];
+var centered = false;
+var myMarker = null;
+
 
 function sendPosition(latitude, longitude){
-    lat = latitude;
-    lon = longitude;
-    socket.emit('position changed', lat, lon);
+        lat = latitude;
+        lon = longitude;
+        socket.emit('position changed', lat, lon);
+    }
+
+function centerMap(latitude, longitude){
+
+    map = L.map('map').setView([latitude, longitude], 10);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    showMyMarker(latitude, longitude);
+    centered = true;
+    //map.panTo(new L.LatLng(latitude, longitude));
+}
+
+function showMyMarker(latitude, longitude){
+    myMarker = L.Marker.movingMarker([[latitude, longitude],[latitude, longitude]], [1000])
+        .bindLabel(username, { noHide: true })
+        .addTo(map);
+   // myMarker.bindPopup(username);
+   // myMarker.bindLabel("NACHO", { noHide: true });
 }
 
 function updateMarker(marker, latitude, longitude){
@@ -24,6 +43,19 @@ function updateMarker(marker, latitude, longitude){
 
 function updatePositions(){
 
+}
+
+function moveRandomMarker(latitude, longitude){
+    if(Math.floor((Math.random() * 10) + 1)<5)
+        moveMyMarker(40,-0.1);
+    else moveMyMarker(latitude, longitude);
+}
+
+function moveMyMarker(latitude, longitude){
+    var newLatLng = new L.LatLng(latitude, longitude);
+    myMarker.moveTo(newLatLng, 1000);
+    //myMarker.setLatLng(newLatLng).update();
+    map.panTo(newLatLng);
 }
 
 $(window).load(function(){
@@ -35,18 +67,20 @@ $(document).ready(function(){
         event.preventDefault();
     });
 });
-
 $("#connect-button").on("click", function(){
-    username = $('#username').val();
 
+    username = $('#username').val();
     if(username){
 
         if ("geolocation" in navigator) {
+
             var watchID = navigator.geolocation.watchPosition(function(position) {
 
+                lat = position.coords.latitude;
+                lon = position.coords.longitude;
+                if(!centered) centerMap(lat, lon);
+                moveRandomMarker(lat, lon);
                 if(socket==null) {
-                    lat = position.coords.latitude;
-                    lon = position.coords.longitude;
                     socket = io("/", {query: 'data=' + username + ';' + position.coords.latitude + ';' + position.coords.longitude});
                     socket.on('user connected', function(username, lat, lon, serverUsers){
                         //DRAW NEW USER
